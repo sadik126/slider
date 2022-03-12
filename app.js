53707a47f81aa4a35b95df400759541c5aefbefc
sadik126 <sadikhimel04@gmail.com>
2022-03-12 20:20:16 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const images = [
+    'images/img-1.jpg',
+    'images/img-2.jpg',
+    'images/img-3.jpg',
+    'images/img-4.jpg',
+    'images/img-5.jpg'
+]
+
+let imgindex = 0;
+
+setInterval(()=>{
+    const imgelement = document.getElementById('imgelement');
+   
+    if(imgindex >= images.length){
+        imgindex = 0;
+    }
+    const imgurl = images[imgindex];
+    imgelement.setAttribute('src',imgurl);
+    console.log(imgurl)
+
+imgindex++;
+},2000)
